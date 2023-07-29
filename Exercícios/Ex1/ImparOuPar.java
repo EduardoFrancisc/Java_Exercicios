67b2c56816b2fb439fdcbea653419cc528665d2e
Dudu <137932110+EduardoFrancisc@users.noreply.github.com>
2023-07-29 14:08:04 -0300

    Update ImparOuPar.java

--- Exercícios/Ex1/ImparOuPar.java
+++ Exercícios/Ex1/ImparOuPar.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class ImparOuPar {
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Digite um numero inteiro");
-        int entrada = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite um numero inteiro");
+            int entrada = scanner.nextInt();
 
-            if (entrada % 2 == 0) {
-                System.out.println("O numero " + entrada + " é par");
-            }else{
-                System.out.println("O numero " + entrada + " é impar");
-            }
+                if (entrada % 2 == 0) {
+                    System.out.println("O numero " + entrada + " é par");
+                }else{
+                    System.out.println("O numero " + entrada + " é impar");
+                }
+        }
     }
 }
+
