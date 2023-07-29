b75fa5b9fd334c3c312c2c9c52cf982cb9c79a1f
Dudu <137932110+EduardoFrancisc@users.noreply.github.com>
2023-07-29 14:05:12 -0300

    Update ImparOuPar.java

--- Exercícios/Ex1/ImparOuPar.java
+++ Exercícios/Ex1/ImparOuPar.java
@@ -1,3 +1,4 @@
+package Exercícios.Ex1;
 import java.util.Scanner;
 public class ImparOuPar {
     public static void main(String[] args){
@@ -8,7 +9,7 @@ public class ImparOuPar {
         int entrada = scanner.nextInt();
 
             if (entrada % 2 == 0) {
-                System.out.println("O numero " + entrada+ " é par");
+                System.out.println("O numero " + entrada + " é par");
             }else{
                 System.out.println("O numero " + entrada + " é impar");
             }
