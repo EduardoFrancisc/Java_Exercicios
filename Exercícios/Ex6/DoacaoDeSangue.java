533266149f328e2f7be5eca720641c7c8bcee9f6
Dudu <137932110+EduardoFrancisc@users.noreply.github.com>
2023-07-30 08:42:04 -0300

    ex6
    ex de simples de if e else completo

---
+++ Exercícios/Ex6/DoacaoDeSangue.java
@@ -0,0 +1,20 @@
+package Exercícios.Ex6;
+import java.util.Scanner;
+
+public class DoacaoDeSangue {
+    public static void main(String[] args){
+        //6 - Para doar sangue é necessário ter entre 18 e 67 anos. Faça um aplicativo que pergunte a idade de uma pessoa e diga se ela pode doar sangue ou não.
+        Scanner scanner = new Scanner(System.in);
+        int idade;
+
+        System.out.println("Qual sua idade?");
+        idade = scanner.nextInt();
+
+        if(idade >= 18 & idade <= 67){
+            System.out.print("Você pode doar sangue");
+        }else{
+            System.out.print("Você não pode doar sangue");
+        }
+        scanner.close();
+    }
+}
